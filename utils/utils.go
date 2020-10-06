6dcd8c79a3103afae6d26f791196158ff3320477
Thái Tấn Lợi <thaitanloi365@gmail.com>
2020-10-06 23:42:55 +0700

    add utils func

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,10 @@
 package utils
 
-import "reflect"
+import (
+	"bytes"
+	"reflect"
+	"text/template"
+)
 
 // IsPtr check pointer
 func IsPtr(val interface{}) bool {
@@ -12,4 +16,63 @@ func IsPtr(val interface{}) bool {
 func IsArr(val interface{}) bool {
 	v := reflect.ValueOf(val)
 	return v.Kind() == reflect.Array
+}
+
+// IsUnix check unix
+func IsUnix(value *int64, nullable ...bool) bool {
+	var canNull = true
+	if len(nullable) > 0 {
+		canNull = nullable[0]
+	}
+
+	if value == nil {
+		return canNull
+	}
+
+	return *value > 0
+}
+
+// IsLat check unix
+func IsLat(value *int64, nullable ...bool) bool {
+	var canNull = true
+	if len(nullable) > 0 {
+		canNull = nullable[0]
+	}
+
+	if value == nil {
+		return canNull
+	}
+
+	var lat = *value
+	return lat >= -90 && lat <= 90
+}
+
+// IsLng check unix
+func IsLng(value *int64, nullable ...bool) bool {
+	var canNull = true
+	if len(nullable) > 0 {
+		canNull = nullable[0]
+	}
+
+	if value == nil {
+		return canNull
+	}
+
+	var lng = *value
+	return lng >= -180 && lng <= 180
+}
+
+// ParseHTML html
+func ParseHTML(htmlFile string, i interface{}) (string, error) {
+	t, err := template.ParseFiles(htmlFile)
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	if err := t.Execute(&tpl, i); err != nil {
+		return "", err
+	}
+
+	return tpl.String(), nil
 }
